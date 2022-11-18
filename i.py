09611686e8b78a6513da8901d8a0ab6d00e80ba9
Nadezhda Kozhedub <lnc3001@gmail.com>
2022-11-18 19:03:34 +0300

    Корректировка

--- i.py
+++ i.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 
-st.header("Анализ тональности текста")
+st.header("Определение тональности текстов")
 st.subheader("Введите текст для анализа")
 
 text = st.text_area(" ",height=50)
