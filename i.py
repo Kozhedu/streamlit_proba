3baf10e59a274022145a84ab023c5c5e5ff8bc02
Nadezhda Kozhedub <lnc3001@gmail.com>
2022-11-19 08:08:16 +0300

    Изменения

--- i.py
+++ i.py
@@ -4,11 +4,10 @@ from transformers import pipeline
 st.header("Определение тональности текстов")
 st.subheader("Введите текст для анализа")
 
-text = st.text_area(" ",height=50)
+text = st.text_area(" ",height=1000)
 
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
-
-
+st.write ("Тональность текста:")
 st.write(classifier(text)[0]["label"])
 
